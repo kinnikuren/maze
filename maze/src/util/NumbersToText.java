72e5ba3dcf20f13e0d2b35b23fd7f46001131ef3
Unmeel Banerjea <ubanerjea@stonewain.com>
2014-09-23 10:28:42 -0400

    cleanup numberstotext

--- maze/src/util/NumbersToText.java
+++ maze/src/util/NumbersToText.java
@@ -4,30 +4,28 @@ public class NumbersToText {
 
     public static String convert(int num) {
         String result = "";
-        switch (num) {
-            case 10: return "ten";
-            case 11: return "eleven";
-            case 12: return "twelve";
-            case 13: return "thirteen";
-            case 14: return "fourteen";
-            case 15: return "fifteen";
-            case 16: return "sixteen";
-            case 17: return "seventeen";
-            case 18: return "eighteen";
-            case 19: return "nineteen";
-        }
-        if (num < 10) {
-            return singleDigits(num);
+        if (num < 10) { return singleDigits(num); }
+        else if (num >= 10 && num < 20) {
+            switch (num) {
+              case 10: return "ten";
+              case 11: return "eleven";
+              case 12: return "twelve";
+              case 13: return "thirteen";
+              case 14: return "fourteen";
+              case 15: return "fifteen";
+              case 16: return "sixteen";
+              case 17: return "seventeen";
+              case 18: return "eighteen";
+              case 19: return "nineteen";
+            }
         }
         else if (num >=20 && num < 30) {
             result = "twenty";
-            if (num > 20) {
-                result = (result + "-" + singleDigits(num%20));
-            }
+            result += num > 20 ? ("-" + singleDigits(num%20)) : "";
         } else {
             result = ((Integer)num).toString();
         }
-        return result;
+      return result;
     }
 
     public static String singleDigits(int num) {
@@ -42,6 +40,6 @@ public class NumbersToText {
         case 8: return "eight";
         case 9: return "nine";
             }
-        return null;
+      return null;
     }
 }
